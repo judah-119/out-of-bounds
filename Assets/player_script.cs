b9bace3d1d4d42e5bdb7ea687c0e39ed593ebf38
judah-119 <judahwlfuchs1@gmail.com>
2026-03-02 17:00:14 -0500

    lol

--- Assets/player_script.cs
+++ Assets/player_script.cs
@@ -5,46 +5,55 @@ using UnityEngine.SceneManagement;
 public class player_script : MonoBehaviour
 {
     [Header("movement")]
-    [SerializeField] Rigidbody2D Rigidbody2D;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed = 8f;
 
-    [Header("stats")]
-    [SerializeField] float speed;
-    [SerializeField] float jumppower;
+    [Header("jump")]
+    [SerializeField] float jumppower = 16f;
     [SerializeField] float variableJumpMultiplier = 0.5f;
     [SerializeField] float fallGravityMultiplier = 1.5f;
+
+    [Header("tilt")]
+    [SerializeField] float maxTiltAngle = 10f;
+    [SerializeField] float tiltSpeed = 10f;
+
+    [Header("jump squash & stretch")]
+    [SerializeField] float jumpSquashX = 1.2f;
+    [SerializeField] float jumpSquashY = 0.8f;
+    [SerializeField] float jumpStretchX = 0.8f;
+    [SerializeField] float jumpStretchY = 1.2f;
+    [SerializeField] float scaleLerpSpeed = 18f;
 
     [Header("ground check")]
     [SerializeField] LayerMask groundlayer;
     [SerializeField] Transform groundcheck;
 
     [Header("timing")]
-    [SerializeField] float coyoteTime = 0.15f;   // NEW
-    [SerializeField] float jumpBufferTime = 0.15f; // NEW
+    [SerializeField] float coyoteTime = 0.15f;
+    [SerializeField] float jumpBufferTime = 0.15f;
 
     [Header("state")]
-    [SerializeField] float Horizontal;
     [SerializeField] SpriteRenderer spriteRenderer;
 
-    // internal timers
+    private float horizontal;
     private float coyoteCounter;
     private float jumpBufferCounter;
-
     private bool jumpHeld;
-    public static player_script Instance;
 
+    private Vector3 normalScale;
+    private Vector3 targetScale;
+
+    public static player_script Instance;
 
     private void Awake()
     {
         if (Instance != null && Instance != this)
-        {
-            Destroy(this);
-
-        }
+            Destroy(gameObject);
         else
-        {
             Instance = this;
-        }
 
+        normalScale = transform.localScale;
+        targetScale = normalScale;
     }
 
     void Update()
@@ -55,29 +64,107 @@ public class player_script : MonoBehaviour
 
     void FixedUpdate()
     {
-        // MOVEMENT
-        Rigidbody2D.linearVelocity = new Vector2(Horizontal * speed, Rigidbody2D.linearVelocity.y);
-
-        // WALK ANIMATION + SPRITE FLIP
-        if (Horizontal < 0)
-        {
-            spriteRenderer.flipX = true;
-        }
-        if (Horizontal > 0)
-        {
-            spriteRenderer.flipX = false;
-        }
-
-        // VARIABLE JUMP HEIGHT + FALL GRAVITY
+        HandleMovement();
         HandleVariableJumpHeight();
         HandleBetterFalling();
-
-        // TRY JUMPING (Buffer + Coyote)
         TryJump();
+        HandleTilt();
+        HandleStretchWhileRising();
+        SmoothScaleReturn();
     }
 
     //=========================================================
-    //                          TIMERS
+    // Movement
+    //=========================================================
+
+    void HandleMovement()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+
+        if (horizontal < 0)
+            spriteRenderer.flipX = true;
+        else if (horizontal > 0)
+            spriteRenderer.flipX = false;
+    }
+
+    //=========================================================
+    // Tilt
+    //=========================================================
+
+    void HandleTilt()
+    {
+        float targetAngle = isgrounded() ? -horizontal * maxTiltAngle : 0f;
+
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, targetAngle);
+
+        transform.rotation = Quaternion.Lerp(
+            transform.rotation,
+            targetRotation,
+            tiltSpeed * Time.fixedDeltaTime
+        );
+    }
+
+    //=========================================================
+    // Jump Logic
+    //=========================================================
+
+    void TryJump()
+    {
+        if (jumpBufferCounter > 0 && coyoteCounter > 0)
+        {
+            // INSTANT SQUASH when jumping
+            transform.localScale = new Vector3(jumpSquashX, jumpSquashY, 1f);
+
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumppower);
+
+            jumpBufferCounter = 0;
+            coyoteCounter = 0;
+        }
+    }
+
+    void HandleStretchWhileRising()
+    {
+        if (!isgrounded() && rb.linearVelocity.y > 0.1f)
+        {
+            targetScale = new Vector3(jumpStretchX, jumpStretchY, 1f);
+        }
+        else
+        {
+            targetScale = normalScale;
+        }
+    }
+
+    void SmoothScaleReturn()
+    {
+        transform.localScale = Vector3.Lerp(
+            transform.localScale,
+            targetScale,
+            scaleLerpSpeed * Time.fixedDeltaTime
+        );
+    }
+
+    void HandleVariableJumpHeight()
+    {
+        if (!jumpHeld && rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity = new Vector2(
+                rb.linearVelocity.x,
+                rb.linearVelocity.y * variableJumpMultiplier
+            );
+        }
+    }
+
+    void HandleBetterFalling()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y *
+                (fallGravityMultiplier - 1f) * Time.fixedDeltaTime;
+        }
+    }
+
+    //=========================================================
+    // Timers
     //=========================================================
 
     void HandleCoyoteTime()
@@ -95,85 +182,47 @@ public class player_script : MonoBehaviour
     }
 
     //=========================================================
-    //                        JUMP LOGIC
+    // Input
     //=========================================================
-
-    void TryJump()
-    {
-        if (jumpBufferCounter > 0 && coyoteCounter > 0)
-        {
-            // Perform jump
-            Rigidbody2D.linearVelocity = new Vector2(Rigidbody2D.linearVelocity.x, jumppower);
-
-            // Reset timers after jump
-            jumpBufferCounter = 0;
-            coyoteCounter = 0;
-        }
-    }
-
-    void HandleVariableJumpHeight()
-    {
-        // Shorten jump when button released
-        if (!jumpHeld && Rigidbody2D.linearVelocity.y > 0)
-        {
-            Rigidbody2D.linearVelocity = new Vector2(
-                Rigidbody2D.linearVelocity.x,
-                Rigidbody2D.linearVelocity.y * variableJumpMultiplier
-            );
-        }
-    }
-
-    void HandleBetterFalling()
-    {
-        if (Rigidbody2D.linearVelocity.y < 0)
-        {
-            Rigidbody2D.linearVelocity += Vector2.up * Physics2D.gravity.y *
-                (fallGravityMultiplier - 1f) * Time.fixedDeltaTime;
-        }
-    }
-
-    //=========================================================
-    //                  INPUT SYSTEM ACTIONS
-    //=========================================================
-
-    private bool isgrounded()
-    {
-        return Physics2D.OverlapCircle(groundcheck.position, 0.2f, groundlayer);
-    }
 
     public void Move(InputAction.CallbackContext context)
     {
-        Horizontal = context.ReadValue<Vector2>().x;
+        horizontal = context.ReadValue<Vector2>().x;
     }
 
-    public void attackpressed(InputAction.CallbackContext context)
-    {
-        Debug.Log("attack pressed");
-    }
     public void Jump(InputAction.CallbackContext context)
     {
-        // Jump pressed
         if (context.started)
         {
             jumpBufferCounter = jumpBufferTime;
             jumpHeld = true;
         }
 
-        // Jump held
         if (context.performed)
             jumpHeld = true;
 
-        // Jump released
         if (context.canceled)
-        {
             jumpHeld = false;
-        }
     }
+
+    public void attackpressed(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            Debug.Log("attack pressed");
+    }
+
     public void restart(InputAction.CallbackContext context)
     {
         if (context.started)
-        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+    }
+
+    //=========================================================
+    // Ground Check
+    //=========================================================
+
+    bool isgrounded()
+    {
+        return Physics2D.OverlapCircle(groundcheck.position, 0.2f, groundlayer);
     }
 }
--- Assets/screen_wrapper.cs
+++ Assets/screen_wrapper.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class ScreenWrap : MonoBehaviour
 {
     private Camera mainCamera;
-    private Vector2 screenBounds;
 
     void Start()
     {
@@ -13,27 +12,50 @@ public class ScreenWrap : MonoBehaviour
     void Update()
     {
         Vector3 viewPos = transform.position;
-
-        float camHeight = mainCamera.orthographicSize;
-        float camWidth = camHeight * mainCamera.aspect;
-
         Vector3 camPos = mainCamera.transform.position;
 
-        float minX = camPos.x - camWidth;
-        float maxX = camPos.x + camWidth;
-        float minY = camPos.y - camHeight;
-        float maxY = camPos.y + camHeight;
+        if (mainCamera.orthographic)
+        {
+            // Keep your old orthographic logic
+            float camHeight = mainCamera.orthographicSize;
+            float camWidth = camHeight * mainCamera.aspect;
 
-        if (viewPos.x > maxX)
-            viewPos.x = minX;
-        else if (viewPos.x < minX)
-            viewPos.x = maxX;
+            float minX = camPos.x - camWidth;
+            float maxX = camPos.x + camWidth;
+            float minY = camPos.y - camHeight;
+            float maxY = camPos.y + camHeight;
 
-        if (viewPos.y > maxY)
-            viewPos.y = minY;
-        else if (viewPos.y < minY)
-            viewPos.y = maxY;
+            Wrap(ref viewPos, minX, maxX, minY, maxY);
+        }
+        else
+        {
+            // Perspective camera logic
+            float distance = Mathf.Abs(viewPos.z - camPos.z);
+
+            float frustumHeight = 2f * distance * Mathf.Tan(mainCamera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+            float frustumWidth = frustumHeight * mainCamera.aspect;
+
+            float minX = camPos.x - frustumWidth / 2f;
+            float maxX = camPos.x + frustumWidth / 2f;
+            float minY = camPos.y - frustumHeight / 2f;
+            float maxY = camPos.y + frustumHeight / 2f;
+
+            Wrap(ref viewPos, minX, maxX, minY, maxY);
+        }
 
         transform.position = viewPos;
+    }
+
+    void Wrap(ref Vector3 pos, float minX, float maxX, float minY, float maxY)
+    {
+        if (pos.x > maxX)
+            pos.x = minX;
+        else if (pos.x < minX)
+            pos.x = maxX;
+
+        if (pos.y > maxY)
+            pos.y = minY;
+        else if (pos.y < minY)
+            pos.y = maxY;
     }
 }
